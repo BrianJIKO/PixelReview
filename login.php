2da0f175a2d78d546a224b08270cec0019853d30
xmendii <javimendozpina@gmail.com>
2024-05-30 23:39:31 -0500

    sss

--- login.php
+++ login.php
@@ -1,15 +1,24 @@
 <?php
 session_start();
 
-$valid_username = 'usuario';
-$valid_password = 'contraseña';
+// Leer el archivo JSON de usuarios
+$users = json_decode(file_get_contents('users.json'), true);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $user_found = false;
 
-    if ($username === $valid_username && $password === $valid_password) {
-        $_SESSION['username'] = $username;
+    // Verificar si el usuario y la contraseña son correctos
+    foreach ($users as $user) {
+        if ($user['username'] === $username && $user['password'] === $password) {
+            $_SESSION['username'] = $username;
+            $user_found = true;
+            break;
+        }
+    }
+
+    if ($user_found) {
         header('Location: index.html');
         exit();
     } else {
