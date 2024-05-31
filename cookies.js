f6813d8cd4c0fcecf81c0666c0b81d4b2ef11ef7
xmendii <javimendozpina@gmail.com>
2024-05-30 22:08:32 -0500

    lil

--- cookies.js
+++ cookies.js
@@ -13,7 +13,7 @@ function setCookie(name, value, days) {
 function getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
-    for(var i=0; i < ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
@@ -22,14 +22,14 @@ function getCookie(name) {
 }
 
 // Manejo del aviso de cookies
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Verificar si las cookies han sido aceptadas
     if (!getCookie("cookiesAccepted")) {
         document.getElementById("cookieConsent").style.display = "block";
     }
 
     // Manejar la aceptación de cookies
-    document.getElementById("acceptCookies").onclick = function() {
+    document.getElementById("acceptCookies").onclick = function () {
         setCookie("cookiesAccepted", "true", 365);
         document.getElementById("cookieConsent").style.display = "none";
     };
